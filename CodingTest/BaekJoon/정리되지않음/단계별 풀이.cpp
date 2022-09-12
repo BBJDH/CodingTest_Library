c7d8a454cf4aa3bd2819e78dac381056b71d1370
봉 봉 <ekffjaos553@naver.com>
2022-09-12 16:59:54 +0900

    소수인펠린드롬 풀이중

--- CodingTest/BaekJoon/정리되지않음/단계별 풀이.cpp
+++ CodingTest/BaekJoon/정리되지않음/단계별 풀이.cpp
@@ -26,6 +26,82 @@ K 2 3 3 3 4 4
 
 */
 
+/*
+1990 - 소수 In 팰린드롬
+
+에라스토테네스의 채로 소수를 먼저 찾고, 이후 팰린드롬수인지를 판별
+
+*/
+#include<iostream>
+#include<bitset>
+#include<vector>
+#include<string>
+using namespace std;
+
+#define Size 100000001
+
+bitset<Size> Nums{};
+vector<string> PrimeNums{};
+
+
+
+void SetPrimeNum()
+{
+	Nums.flip();
+	Nums[0] = false;
+	Nums[1] = false;
+	for (int i = 2; i * i < Size; i++)
+	{
+		if (Nums[i])
+			PrimeNums.push_back(to_string(i));
+		for (int j = 2; i * j < Size; j++)
+		{
+			Nums[i * j] = false;
+		}
+	}
+}
+
+bool CheckPalindrome(int const Num)
+{
+	string Str = to_string(Num);
+
+	for (int i = 0; i < Str.size() / 2; i++)
+	{
+		if (Str[i] != Str[Str.size() - i - 1])
+			return false;
+	}
+
+	return true;
+}
+
+void GetPalindrome(int const StartNum, int const EndNum)
+{
+	for (int i = StartNum; i <= EndNum; i++)
+	{
+		if (Nums[i] and CheckPalindrome(i))
+		{
+			cout << i << "\n";
+		}
+	}
+	cout << -1;
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int StartNum = 0;
+	int EndNum = 0;
+
+	cin >> StartNum >> EndNum;
+	SetPrimeNum();
+	GetPalindrome(StartNum, EndNum);
+
+
+	return 0;
+}
+
 
 #pragma region 골드
 /*
