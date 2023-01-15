a00d9cd5b1029a00c2006e7becd438e57a3ad17a
봉 봉 <ekffjaos553@naver.com>
2023-01-15 11:18:50 +0900

    [백준/C++] Show Me The Code A번 B번 코드가 서로 바뀜 다시 수정

--- CodingTest/BaekJoon/대회/ShowMeTheCode - 3회차 코딩테스트/A번 - 신을 모시는 사당.cpp
+++ CodingTest/BaekJoon/대회/ShowMeTheCode - 3회차 코딩테스트/A번 - 신을 모시는 사당.cpp
@@ -9,96 +9,63 @@
  *
  */
 
-
 #include <iostream>
-#include <queue>
 #include <vector>
 
 using namespace std;
 
-struct _int2
-{
-	int X;
-	int Y;
-};
-
-queue<_int2> BFS_Queue{};
-vector<_int2> EmptyPlace{};
-_int2 Direction[4] = { {1,0}, {-1,0}, {0,-1}, {0,1} };
-int Map[1000][1000]{};
-bool Visit[1000][1000]{};
-int SizeY = 0, SizeX = 0;
-
-bool Push(_int2 const & Next)
-{
-	if(Visit[Next.Y][Next.X]==false)
-	{
-		Visit[Next.Y][Next.X] = true;
-		BFS_Queue.push(Next);
-		return true;
-	}
-	return false;
-}
-
-void RoundEdges(_int2 const & Current)
-{
-	for(int i=0; i<4; i++)
-	{
-		_int2 Next{};
-		//0이하로 내려가는경우
-		Next.X = Current.X + Direction[i].X;
-		Next.Y = Current.Y + Direction[i].Y;
-		if (Next.X == -1)
-			Next.X = SizeX - 1;
-		else if(Next.X == SizeX)
-			Next.X = 0;
-
-		if (Next.Y == -1)
-			Next.Y = SizeY - 1;
-		else if (Next.Y == SizeY)
-			Next.Y = 0;
-		Push(Next);
-
-	}
-}
-
-bool BFS(_int2 const & Start)
-{
-	if (Push(Start) == false)
-		return false;
-
-	while (!BFS_Queue.empty())
-	{
-		_int2 const Current = BFS_Queue.front();
-		BFS_Queue.pop();
-		RoundEdges(Current);
-	}
-	return true;
-}
-
+vector<int>Nums{};
+int DP_Min[100000]{};
+int DP_Max[100000]{};
 int main()
 {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	cin >> SizeY >> SizeX;
-	for(int y=0; y<SizeY; y++)
+	int N = 0;
+	cin >> N;
+	int PrevNum = 0;
+	int Sum = 0;
+
+	for (int i = 0; i < N; i++)
 	{
-		for(int x=0; x<SizeX; x++)
+		int InputNum = 0;
+		cin >> InputNum;
+		if (PrevNum != InputNum)
 		{
-			cin >> Visit[y][x];
-			if(Visit[y][x]==0)
+			if (PrevNum == 0)
 			{
-				EmptyPlace.push_back({x,y});
+				PrevNum = InputNum;
+				Sum++;
+				continue;
 			}
+			if (PrevNum == 2)
+				Sum *= -1;
+			Nums.push_back(Sum);
+			PrevNum = InputNum;
+			Sum = 0;
 		}
+		Sum++;
 	}
-	int Result = 0;
-	for(auto const & elem : EmptyPlace)
+	if (PrevNum == 2)
+		Sum *= -1;
+	Nums.push_back(Sum);
+	//음수 양수로 치환 완료, 최소 최대 부분수열 구하기 시작
+
+	DP_Max[0] = Nums[0];
+	DP_Min[0] = Nums[0];
+	int MaxNum = DP_Max[0];
+	int MinNum = DP_Min[0];
+	for (int i = 1; i < Nums.size(); i++)
 	{
-		if (BFS(elem))
-			Result++;
+		DP_Max[i] = max(DP_Max[i - 1] + Nums[i], Nums[i]);
+		DP_Min[i] = min(DP_Min[i - 1] + Nums[i], Nums[i]);
+		MaxNum = max(MaxNum, DP_Max[i]);
+		MinNum = min(MinNum, DP_Min[i]);
 	}
-	cout << Result;
+	cout << max(abs(MinNum), MaxNum);
 	return 0;
 }
+
+
+
--- CodingTest/BaekJoon/대회/ShowMeTheCode - 3회차 코딩테스트/B번 - 도넛 행성.cpp
+++ CodingTest/BaekJoon/대회/ShowMeTheCode - 3회차 코딩테스트/B번 - 도넛 행성.cpp
@@ -10,63 +10,98 @@
  *BFS 탐색을 진행하며 해당 구역이 새로운 구역이라면 카운트하여 출력한다.
  *
  */
+
+
+
 #include <iostream>
+#include <queue>
 #include <vector>
 
 using namespace std;
 
-vector<int>Nums{};
-int DP_Min[100000]{};
-int DP_Max[100000]{};
+struct _int2
+{
+	int X;
+	int Y;
+};
+
+queue<_int2> BFS_Queue{};
+vector<_int2> EmptyPlace{};
+_int2 Direction[4] = { {1,0}, {-1,0}, {0,-1}, {0,1} };
+int Map[1000][1000]{};
+bool Visit[1000][1000]{};
+int SizeY = 0, SizeX = 0;
+
+bool Push(_int2 const& Next)
+{
+	if (Visit[Next.Y][Next.X] == false)
+	{
+		Visit[Next.Y][Next.X] = true;
+		BFS_Queue.push(Next);
+		return true;
+	}
+	return false;
+}
+
+void RoundEdges(_int2 const& Current)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		_int2 Next{};
+		//0이하로 내려가는경우
+		Next.X = Current.X + Direction[i].X;
+		Next.Y = Current.Y + Direction[i].Y;
+		if (Next.X == -1)
+			Next.X = SizeX - 1;
+		else if (Next.X == SizeX)
+			Next.X = 0;
+
+		if (Next.Y == -1)
+			Next.Y = SizeY - 1;
+		else if (Next.Y == SizeY)
+			Next.Y = 0;
+		Push(Next);
+
+	}
+}
+
+bool BFS(_int2 const& Start)
+{
+	if (Push(Start) == false)
+		return false;
+
+	while (!BFS_Queue.empty())
+	{
+		_int2 const Current = BFS_Queue.front();
+		BFS_Queue.pop();
+		RoundEdges(Current);
+	}
+	return true;
+}
+
 int main()
 {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	int N = 0;
-	cin >> N;
-	int PrevNum = 0;
-	int Sum = 0;
-
-	for (int i = 0; i < N; i++)
+	cin >> SizeY >> SizeX;
+	for (int y = 0; y < SizeY; y++)
 	{
-		int InputNum = 0;
-		cin >> InputNum;
-		if (PrevNum != InputNum)
+		for (int x = 0; x < SizeX; x++)
 		{
-			if (PrevNum == 0)
+			cin >> Visit[y][x];
+			if (Visit[y][x] == 0)
 			{
-				PrevNum = InputNum;
-				Sum++;
-				continue;
+				EmptyPlace.push_back({ x,y });
 			}
-			if (PrevNum == 2)
-				Sum *= -1;
-			Nums.push_back(Sum);
-			PrevNum = InputNum;
-			Sum = 0;
 		}
-		Sum++;
 	}
-	if (PrevNum == 2)
-		Sum *= -1;
-	Nums.push_back(Sum);
-	//음수 양수로 치환 완료, 최소 최대 부분수열 구하기 시작
-
-	DP_Max[0] = Nums[0];
-	DP_Min[0] = Nums[0];
-	int MaxNum = DP_Max[0];
-	int MinNum = DP_Min[0];
-	for (int i = 1; i < Nums.size(); i++)
+	int Result = 0;
+	for (auto const& elem : EmptyPlace)
 	{
-		DP_Max[i] = max(DP_Max[i - 1] + Nums[i], Nums[i]);
-		DP_Min[i] = min(DP_Min[i - 1] + Nums[i], Nums[i]);
-		MaxNum = max(MaxNum, DP_Max[i]);
-		MinNum = min(MinNum, DP_Min[i]);
+		if (BFS(elem))
+			Result++;
 	}
-	cout << max(abs(MinNum), MaxNum);
+	cout << Result;
 	return 0;
 }
-
-
-
