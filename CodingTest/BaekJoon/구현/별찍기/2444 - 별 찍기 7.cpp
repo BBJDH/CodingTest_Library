d6ad5cd0d4ab4caa32a27046838977af68397ba8
봉 봉 <ekffjaos553@naver.com>
2023-03-18 21:31:48 +0900

    2444 - 별 찍기 7 풀이

---
+++ CodingTest/BaekJoon/구현/별찍기/2444 - 별 찍기 7.cpp
@@ -0,0 +1,46 @@
+/*
+ *2444 - 별 찍기 7
+ *
+ *0000*			- b 4 s 1
+ *000***		- b 3 s 3
+ *00*****		- b 2 s 5
+ *0*******		- b 1 s 7
+ **********		- b 0 s 9
+ *
+ */
+
+ //#include<iostream>
+ //using namespace std;
+ //
+ //int main()
+ //{
+ //	int CountOfLine =0;
+ //	cin >> CountOfLine;
+ //
+ //	for(int blank = CountOfLine-1; blank>=0; blank--)
+ //	{
+ //		for(int i=0;i< blank; i++)
+ //		{
+ //			cout << " ";
+ //		}
+ //		for (int i = 0; i < (CountOfLine- blank)*2-1; i++)
+ //		{
+ //			cout << "*";
+ //		}
+ //		cout << "\n";
+ //	}
+ //	//역방향
+ //	for (int blank = 1; blank < CountOfLine ; blank++)
+ //	{
+ //		for (int i = 0; i < blank; i++)
+ //		{
+ //			cout << " ";
+ //		}
+ //		for (int i = 0; i < (CountOfLine - blank) * 2 - 1; i++)
+ //		{
+ //			cout << "*";
+ //		}
+ //		cout << "\n";
+ //	}
+ //
+ //}
